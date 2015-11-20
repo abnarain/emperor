2b5f1e3270b47d654953a0e44416d8afffa9ee8c
Abhinav Narain <abhinavnarain10@gmail.com>
2015-11-20 15:30:29 -0800

    with new ppm encoding

--- message_on_wire_gen.py
+++ message_on_wire_gen.py
@@ -18,7 +18,7 @@ def return_sample_messages():
    #message_to_encode_2 = b'I am going to use RS Encoder. There is basically so much to do that I cannot believe. We need to do as much we can how much we can and keep doing it unless we get success in it and that is how the world works. Lets do it...  We need to try out a bigger string to transmit and check how the decoder performs particularly with twice the size of the string that we started in the first try of scewing things up and hope things go well nasty well this time around even with a bigger string.'
 
     #message_to_encode = b'I am going to use RS Encoder. There is basically so much to do that I cannot believe. We need to do as much we can how much we can and keep doing it unless we get success in it and that is how the world works. Lets do it...  We need to try out a bigger string to transmit and check how the decoder performs particularly with twice the size of the string that we started in the first try of scewing things up and hope things go well nasty well this time around even with a bigger string. This is a slightly bigger and mostly well done text because this will test the things of the RS code, the channel and the scheme in terms of the secrecy as the number of slots to spread the information bits will grow polynomially in terms of number of bits of input. This is also a penultimate vector to deal with unless we do things for even more killer long vector as the final one. There are plenty of experiments to be done right now after this basic step goes in of generating test vectors to transmit and receive over the powerline communication channel.'
-    #message_to_encode = b'Abhinav ' #Narain good' ##is a' good person'
+    #message_to_encode = b'Abhinav Narain is good'
     return message_to_encode
 
 
@@ -71,11 +71,12 @@ def ppm_modulation(message_length_on_wire,message_amp_representation, separated_
             counter +=1
             #print i,
             if message_amp_representation[index]==1:
-                new_indexes.append([j,11])
+                new_indexes.append([j,110])
                 tx_instances.append(1)
                 tx_instances.append(1)
-                j=j+2
-                counter_3 +=2
+                tx_instances.append(0)
+                j=j+3
+                counter_3 +=3
                 index = index+1
             elif message_amp_representation[index]==0:
                 new_indexes.append([j,101])
@@ -95,16 +96,16 @@ def ppm_modulation(message_length_on_wire,message_amp_representation, separated_
 
     a = np.array(new_indexes, dtype=np.int64)
     #a.astype('int64/float32').tofile('transmission_indexes.dat')
-    np.save('transmission_indexes_to_compare_3.dat',a)
+    np.save('test_transmission_indexes_to_compare_1.dat',a)
     tx_instances=preamble+tx_instances + [0]*10000+[1]
 
     b = np.array(tx_instances, dtype=np.float32)
-    b.astype('float32').tofile('preamble_new_transmission_3.dat')
+    b.astype('float32').tofile('test_preamble_new_tx_1.dat')
     print "index= " ,index
-    np.save('save_preamble_new_transmission_3.dat',b)
+    np.save('test_save_preamble_new_tx_1.dat',b)
     print "tx instances len (final len)= ",len(tx_instances)
-    print "times sep tx was found(2040 spots) : ", counter
-    print "rest of time spots(416100 -counter= ", counter_2
+    print "times sep tx was found (length of loop) : ", counter
+    print "rest of time spots",tmp_len ," - ",counter, "= ", counter_2
     print "bit1 added due to #1s ", counter_3
     print "bit1  added due to #0s ",counter_4
 
@@ -132,6 +133,5 @@ if __name__=='__main__':
     print "duplicates are ", [item for item, count in collections.Counter(separated_tx).items() if count > 1]
     message_amp_representation= [int(i) for i in list(bit_representation_message)]
     ppm_modulation(message_length_on_wire,message_amp_representation, separated_tx)
-    print " must be = msg len on wire= ", message_length_on_wire
 
 
