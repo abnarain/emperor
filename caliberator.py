e63f3ea515589d0bdcf6726ca58d06f654810a40
Abhinav Narain <abhinavnarain10@gmail.com>
2015-12-04 01:42:27 -0800

    adding caliberation of specific files

--- caliberator.py
+++ caliberator.py
@@ -1,78 +1,47 @@
-import numpy as np
 import scipy, sys, getopt
-import matplotlib.font_manager
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib import pyplot
-
-
-
-fig_width = 10
-fig_length = 10.25
-# Can be used to adjust the border and spacing of the figure
-fig_left = 0.12
-fig_right = 0.94
-fig_bottom = 0.25
-fig_top = 0.94
-fig_hspace = 0.5
-
-
-def plotter(x,y, outfile_name,title,xlabel,ylabel):
-    fig = Figure(linewidth=0.0)
-    fig.set_size_inches(fig_width,fig_length, forward=True)
-    Figure.subplots_adjust(fig, left = fig_left, right = fig_right, bottom = fig_bottom, top = fig_top, hspace = fig_hspace)
-    _subplot = fig.add_subplot(1,1,1)
-    _subplot.plot(x, label=xlabel)
-    _subplot.legend()
-    _subplot.set_title(title,fontsize=17)
-    canvas = FigureCanvasAgg(fig)
-    if '.pdf' in outfile_name:
-        canvas.print_figure(outfile_name, dpi = 110)
-    if '.png' in outfile_name:
-        canvas.print_figure(outfile_name, dpi = 110)
-
 
 def main(argv):
-    inputfile=''
-    ftype=''
-    compare_file=''
+    noise_file=''
+    ones_file=''
+    noiseflag,onesflag=0,0
     try:
-        opts, args = getopt.getopt(argv,"h:i:f::o",["ifile=","ftype=","ofile"])
+        opts, args = getopt.getopt(argv,"h:n:o",["nfile=","ofile="])
     except getopt.GetoptError:
-        print 'file.py -i <inputfile> -f <caption on graph>'
+        print 'file.py -i <noisefile> -o <onesfile>'
         sys.exit(2)
     for opt, arg in opts:
         print opt ,arg,
         if opt == '-h':
-            print 'file.py -i <inputfile> -o <compare_file> -f <caption on graph> '
+            print 'file.py -n <noisefile> -o <onesfile> '
             sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-f", "--ftype"):
-            ftype = arg
-        elif opt in ("-o", "--ftype"):
-            compare_file = arg
+        elif opt in ("-n", "--nfile"):
+            noisefile = arg
+            noiseflag=1
+        elif opt in ("-o", "--ofile"):
+            onesfile = arg
+            noiseflag =1
         else:
             print "check help for usage" 
             sys.exit()
 
-
-    f = scipy.fromfile(open(inputfile), dtype=scipy.float32)
     false_positives, false_negatives=0,0
     negative =0
-    if negative ==1:
-        for i in range(0,len(f)):
+
+    if noiseflag ==1:
+        print " Noise caliberation "
+        noise= scipy.fromfile(open(onesfile), dtype=scipy.float32)
+        for i in range(0,len(noise)):
+            if noise[i] == 1.0 :
+                false_positives += 1
+        print "false positives ",false_positives, len(f), false_positives*1.0/len(f)
+    if onesflag==1:
+        print "Training caliberation "
+        ones= scipy.fromfile(open(onesfile), dtype=scipy.float32)
+        for i in range(0,len(ones)):
             if f[i] == 0.0 :
                 false_negatives += 1
-    elif negative ==0:
-        for i in range(0,len(f)):
-            if f[i] == 1.0 :
-                false_positives += 1
-    if negative ==1:            
         print "\nfalse negatives ",false_negatives, len(f), false_negatives*1.0/len(f)
-    elif negative ==0:
-        print "false positives ",false_positives, len(f), false_positives*1.0/len(f)
-    #plotter(measured_data,[], "measured_data.pdf","plotting the data","index","y value")
+
 
 if __name__=='__main__':
     main(sys.argv[1:])
