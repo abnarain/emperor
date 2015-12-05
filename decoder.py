c46f9d50eca0300fa3640f1a21b6f39db17a6759
Abhinav Narain <abhinavnarain10@gmail.com>
2015-12-04 16:06:58 -0800

    Adding ber calculations for single pulse-bit decoder

--- decoder.py
+++ decoder.py
@@ -38,25 +38,29 @@ def ber_repetition(oracle_indices,to_decode_data):
     print "False Positives = ", false_positives
     print "False Negatives = ", false_negatives
 
-def ber_single(oracle_indices,to_decode_data):
+def ber_single(oracle_indices,to_decode_data, original_message):
     ppm= oracle_indices
-    print "length of oracle indices/bits to transmit ", len(oracle_indices), "len of to_decode_data",len(to_decode_data)
-    print ppm[:10]
+    to_decode_data = to_decode_data[:len(original_message)]
+    print "\nLast element oracle: ", oracle_indices[-1], "len of to_decode_data ",len(to_decode_data), "messsage size ", len(original_message)
+    xored_data=[]
+    false_negatives,false_positives =0,0
     for i in range(0,len(ppm)):
         tup=ppm[i]
         idx=tup[0].astype(np.int64)
+        print " idx = ", idx,
         if tup[1]==100:
             xored_data.append(int(to_decode_data[idx]) ^ int(original_message[idx]))
-            if original_message[i] == 1 and  to_decode_data[i] == 0 :
+            if original_message[idx] == 1 and  to_decode_data[idx] == 0 :
                 false_negatives += 1
         elif tup[1] ==000:
-            if original_message[i] == 0 and  to_decode_data[i] == 1 :
-                false_positives += 1
             xored_data.append(int(to_decode_data[idx]) ^ int(original_message[idx]))
+            if original_message[idx] == 0 and  to_decode_data[idx] == 1 :
+                false_positives += 1
         else:
             print "Impossible!"
     print "False Positives = ", false_positives
     print "False Negatives = ", false_negatives
+    print "Bit Error Rate = ", sum(xored_data)*1.0/len(xored_data)
 
 def start_index(to_decode_file):
     cor1 =  np.correlate(to_decode_file,preamble,"full")
@@ -66,7 +70,7 @@ def start_index(to_decode_file):
         if  cor1[i]==maximum:
             min_index_of_max= i
             break
-    print "value of the mean index is ", min_index_of_max, "len(preamble)-min_index_of_max: ",len(preamble)-  min_index_of_max
+    print "value of the mean index is ", min_index_of_max
     return min_index_of_max
 
 def decoding_maj2(oracle_indices,to_decode_data):
@@ -138,7 +142,7 @@ def decoding_maj3(oracle_indices,to_decode_data):
 def single_demod(oracle_indices,to_decode_data):
     ppm= oracle_indices
     rs_decoder_input=[]
-    print "length of total received array is " , len(to_decode_data), "original length of transmissions", len(oracle_indices)
+    print "\nLength of total received array is " , len(to_decode_data), "original length of transmissions", len(oracle_indices)
     for i in range(0,len(ppm)):
         tup=ppm[i]
         idx=tup[0].astype(np.int64)
@@ -165,8 +169,6 @@ def single_demod(oracle_indices,to_decode_data):
         print chr(ord(sx)),
         bin_rep_to_decode.extend(sx)
     return bin_rep_to_decode
-
-
 
 
 def main(argv):
@@ -210,13 +212,16 @@ def main(argv):
     to_decode_data1= to_decode_file[start_data_index:]
     to_decode_data= to_decode_data1.astype(np.int64)
     #print "lengths of data going in:", len(original_message), len(to_decode_data)
-    #error_rate_calculations(to_decode_data, original_message)
-    bin_rep_to_decode=decoding_maj3(oracle_indices,to_decode_data)
-    #bin_rep_to_decode=single_demod(oracle_indices,to_decode_data)
-    #bin_rep_to_decode=decoding_byte_array2(oracle_indices,to_decode_data)
+    #bin_rep_to_decode=decoding_maj3(oracle_indices,to_decode_data)
+    bin_rep_to_decode=single_demod(oracle_indices,to_decode_data)
+    ber_single(oracle_indices,to_decode_data, original_message)
     print "\nGoing to decode"
     rs= reedsolo.RSCodec(32)
-    message_decoded = rs.decode(bin_rep_to_decode)
+    message_decoded =''
+    try:
+        message_decoded = rs.decode(bin_rep_to_decode)
+    except:
+        print "Cannot decode using RS decoder " 
     print "decoded message is ",message_decoded
     print "\n"
 
