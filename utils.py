3e258c97e5b0d983f10a7d3961f4983e14fd0ee7
Abhinav Narain <abhinavnarain10@gmail.com>
2016-01-12 02:01:21 -0800

    adding support for pickling

--- utils.py
+++ utils.py
@@ -2,8 +2,10 @@ import scipy, sys, getopt, pickle
 from scipy.stats import norm,rayleigh, expon, entropy
 import numpy as np
 
-
-
+'''
+This file does the caliberation for the file containing bits for noise.
+This file also pickles the iq file statistics
+'''
 def movingaverage(interval, window_size=512):
     window = np.ones(int(window_size))/float(window_size)
     return np.convolve(interval, window, 'same')
@@ -35,12 +37,13 @@ def distribution_statistic(data,bins):
 def main(argv):
     noisefile=''
     onesfile=''
-    pmode,noiseflag,onesflag=0,0,0
+    mesgfile=''
+    pmode,noiseflag,onesflag,msgflag=0,0,0,0
     
     try:
-        opts, args = getopt.getopt(argv,"h:n:o:p:",["nfile=","ofile=","pmode="])
+        opts, args = getopt.getopt(argv,"h:n:o:m:p:",["nfile=","ofile=","mfile","pmode="])
     except getopt.GetoptError:
-        print 'file.py -i <noisefile> -o <onesfile> -p <mode on/off>'
+        print 'file.py -i <noisefile> -o <onesfile> -m <mesgfile> -p <mode on/off>'
         sys.exit(2)
     for opt, arg in opts:
         print opt ,arg,
@@ -52,32 +55,53 @@ def main(argv):
             noiseflag=1
         elif opt in ("-o", "--ofile"):
             onesfile = arg
-            onesflag =1
+            onesflag =
+        elif opt in ("-m", "--mfile"):
+            mesgfile = arg
+            mesgflag =1
         elif opt in ("-p", "--pmode"):
             pmode= 1
         else:
             print "check help for usage" 
             sys.exit()
     if pmode ==1:
+        '''
+        This mode is for dealing with IQ samples instead of reading bit files. This will be useful for the better.
+        Get the histogram and curve fitting parameters and save them in pickle.
+        '''
         if noiseflag ==1:
             nl=noisefile.split('_')
-            Noisepickle= '_'.join([ nl[-4][-5:], nl[-3][-2:], nl[-2], nl[-1][:-4]])
-            print " In noise caliberation "
+            #Noisepickle= '_'.join([ nl[-4][-5:], nl[-3][-2:], nl[-2], nl[-1][:-4]])
+            Noisepickle= '_'.join(['noise' ,nl[-2]])
+            print " In noise pickling "
             z_noise= scipy.fromfile(open(noisefile), dtype=scipy.complex64)
             noise=map(np.absolute,z_noise)
             noise_hist, noise_bins= np.histogram(noise,200,density=1)
             [noise_exp_param, noise_pdf_exp_fitted, noise_rayleigh_param, noise_pdf_rayleigh_fitted] = distribution_statistic(noise, noise_bins)
-            pickle.dump([noise_exp_param, noise_pdf_exp_fitted, noise_rayleigh_param, noise_pdf_rayleigh_fitted], open(Noisepickle+'.pickle','wb'))
+            pickle.dump([noise_hist, noise_bins, noise_exp_param, noise_pdf_exp_fitted, noise_rayleigh_param, noise_pdf_rayleigh_fitted], open(Noisepickle+'.pickle','wb'))
+
+        if msgflag ==1:
+            print "in mesg pickling "
+            ol=mesgsfile.split('_')        
+            #Mesgspickle= '_'.join([ ol[-4][-4:], ol[-3][-2:], ol[-2], ol[-1][:-4]])
+            Mesgpickle='_'.join([ol[-4][-4:],ol[-3],ol[-2]]);
+            z_mesg= scipy.fromfile(open(mesgfile), dtype=scipy.complex64)
+            mesg=map(np.absolute,z_mesg)
+            mesg_hist, mesg_bins= np.histogram(mesg,200,density=1)
+            [mesg_exp_param, mesg_pdf_exp_fitted, mesg_rayleigh_param, mesg_pdf_rayleigh_fitted] = distribution_statistic(mesg, mesg_bins)
+            pickle.dump([mesg_hist, mesg_bins, mesg_exp_param, mesg_pdf_exp_fitted, mesg_rayleigh_param, mesg_pdf_rayleigh_fitted], open(Mesgpickle+'.pickle','wb'))
+    
         if onesflag ==1:
             print "in ones caliberation "
             ol=onesfile.split('_')        
-            Onespickle= '_'.join([ ol[-4][-4:], ol[-3][-2:], ol[-2], ol[-1][:-4]])
+            #Onespickle= '_'.join([ ol[-4][-4:], ol[-3][-2:], ol[-2], ol[-1][:-4]])
+            Onespickle='_'.join(['ones',ol[-3],ol[-2]]);
             z_ones= scipy.fromfile(open(onesfile), dtype=scipy.complex64)
             ones=map(np.absolute,z_ones)
             ones_hist, ones_bins= np.histogram(ones,200,density=1)
             [ones_exp_param, ones_pdf_exp_fitted, ones_rayleigh_param, ones_pdf_rayleigh_fitted] = distribution_statistic(ones, ones_bins)
-            pickle.dump([ones_exp_param, ones_pdf_exp_fitted, ones_rayleigh_param, ones_pdf_rayleigh_fitted], open(Onespickle+'.pickle','wb'))
-
+            pickle.dump([ones_hist, ones_bins, ones_exp_param, ones_pdf_exp_fitted, ones_rayleigh_param, ones_pdf_rayleigh_fitted], open(Onespickle+'.pickle','wb'))
+        
         print "Done with Pickle mode "
         sys.exit(1)
 
@@ -90,6 +114,7 @@ def main(argv):
             if noise[i] == 1.0 :
                 false_positives += 1
         print "false positives ",false_positives, len(noise), false_positives*1.0/len(noise)
+    #this following code is not needed for caliberation of false negative is not this way.    
     if onesflag==1:
         print "Training caliberation "
         ones= scipy.fromfile(open(onesfile), dtype=scipy.float32)
@@ -111,5 +136,7 @@ def main(argv):
 
 if __name__=='__main__':
     main(sys.argv[1:])
+
+
 
 
